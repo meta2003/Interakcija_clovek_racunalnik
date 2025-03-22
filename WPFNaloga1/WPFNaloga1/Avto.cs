86cc2b1672de05b6fe20f39d3400d70c9ed0f5b5
Metod Golob <metod.golob.2003@gmail.com>
2025-03-22 14:38:26 +0100

    init

---
+++ WPFNaloga1/WPFNaloga1/Avto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFNaloga1
+{
+   public class Avto : INotifyPropertyChanged
+    {
+        private string _znamka;
+        private string _model;
+        private int _leto;
+        private string _slika;
+
+        public string Znamka { get => _znamka; set { _znamka = value; OnPropertyChanged(); } }
+        public string Model { get => _model; set { _model = value; OnPropertyChanged(); } }
+        public int Leto { get => _leto; set { _leto = value; OnPropertyChanged(); } }
+        public string Slika { get => _slika; set { _slika = value; OnPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WPFNaloga1/WPFNaloga1/AvtoViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFNaloga1
+{
+    public class AvtoViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Avto> Avtomobili { get; set; } = new ObservableCollection<Avto>();
+        public ICommand DodajAvtoCommand { get; }
+        public ICommand OdstraniAvtoCommand { get; }
+        private Avto _izbraniAvto;
+
+        public Avto IzbraniAvto
+        {
+            get => _izbraniAvto;
+            set { _izbraniAvto = value; OnPropertyChanged(); }
+        }
+
+        public AvtoViewModel()
+        {
+            DodajAvtoCommand = new RelayCommand(o => DodajAvto());
+            OdstraniAvtoCommand = new RelayCommand(o => OdstraniAvto(), o => IzbraniAvto != null);
+        }
+
+        private void DodajAvto()
+        {
+            Avtomobili.Add(new Avto { Znamka = "VW", Model = "Golf", Leto = 2020, Slika = "golf.jpg" });
+        }
+
+        private void OdstraniAvto()
+        {
+            if (IzbraniAvto != null)
+                Avtomobili.Remove(IzbraniAvto);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
